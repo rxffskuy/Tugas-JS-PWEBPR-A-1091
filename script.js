e346b7b45772d59b49654db192c71ce6d33f2bbb
rxffskuy <146223904+rxffskuy@users.noreply.github.com>
2024-03-30 01:15:54 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function heem(){
+    document.getElementById("jdl").textContent="Anda Berbohong!"
+    document.getElementById("bar").src="image 3.png"
+    document.getElementById("kotak").style.backgroundColor = "#E12E2E"
+    document.getElementById("tulisan").textContent="Anda adalah seorang Teknisi"
+    document.getElementById("tmbl").style.display="none"
+    
+}
+
+function tidak(){
+    document.getElementById("jdl").textContent="Anda Benar!"
+    document.getElementById("bar").src="image 2.png"
+    document.getElementById("kotak").style.backgroundColor = "#6FD240"
+    document.getElementById("tulisan").textContent="Anda adalah seorang Mahasiswa"
+    document.getElementById("tmbl").style.display="none"
+}
+
